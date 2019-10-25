246fd32e7294bdc9423a75997e6d474a5c8c0b2d
Aaron Voelker <arvoelke@gmail.com>
2019-10-25 10:09:52 -0400

    Added proprietary ABR license

--- lmu/__init__.py
+++ lmu/__init__.py
@@ -1,1 +1,5 @@
 from .lmu import Legendre, InputScaled, LMUCell, LMUCellODE, LMUCellGating
+
+__copyright__ = "2019, Applied Brain Research"
+__license__ = "Free for non-commercial use; see LICENSE.rst"
+__version__ = "0.1"
