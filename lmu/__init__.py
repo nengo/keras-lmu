b56bb3e55f95db0690331b7f5bdda0f987aa93e0
samir-alazzam <samiralazzam7@gmail.com>
2020-05-15 11:04:36 -0400

    Added third versioning level

--- lmu/__init__.py
+++ lmu/__init__.py
@@ -2,4 +2,4 @@ from .lmu import Legendre, InputScaled, LMUCell, LMUCellODE, LMUCellGating
 
 __copyright__ = "2019, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
-__version__ = "0.1"
+__version__ = "0.1.0"
