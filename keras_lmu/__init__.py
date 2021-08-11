6e41576c943f2788b0033c5dac61f6832540555a
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2021-08-11 12:16:50 -0300

    Use nengo-bones to template version.py

--- keras_lmu/__init__.py
+++ keras_lmu/__init__.py
@@ -1,7 +1,7 @@
 """KerasLMU provides a package for deep learning with Legendre Memory Units."""
 
 from .layers import LMU, LMUCell, LMUFeedforward
+from .version import copyright as __copyright__
 from .version import version as __version__
 
-__copyright__ = "2019-2021, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
