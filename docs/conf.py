83b9eec2cb13f612012a04048c57756ff6aac93b
NengoBones <info@appliedbrainresearch.com>
2024-02-28 20:12:37 +0000

    Automatic update to NengoBones 01e4432
    01e4432 - Add cache option to setup action

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ extensions = [
     "sphinx.ext.todo",
     "nbsphinx",
     "nengo_sphinx_theme",
-    "nengo_sphinx_theme.ext.backoff",
     "nengo_sphinx_theme.ext.redirects",
     "nengo_sphinx_theme.ext.sourcelinks",
     "notfound.extension",
@@ -89,7 +88,7 @@ autodoc_inherit_docstrings = False
 
 project = "KerasLMU"
 authors = "Applied Brain Research"
-copyright = "2019-2023 Applied Brain Research"
+copyright = "2019-2024 Applied Brain Research"
 version = ".".join(keras_lmu.__version__.split(".")[:2])  # Short X.Y version
 release = keras_lmu.__version__  # Full version, with tags
 
@@ -120,7 +119,7 @@ html_theme_options = {
          var _paq = window._paq = window._paq || [];
          _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
          _paq.push(["setCookieDomain", "*.appliedbrainresearch.com"]);
-         _paq.push(["setDomains", ["*.appliedbrainresearch.com","*.edge.nengo.ai","*.forum.nengo.ai","*.labs.nengo.ai","*.nengo.ai"]]);
+         _paq.push(["setDomains", ["*.appliedbrainresearch.com","*.edge.nengo.ai","*.forum.nengo.ai","*.nengo.ai"]]);
          _paq.push(["enableCrossDomainLinking"]);
          _paq.push(["setDoNotTrack", true]);
          _paq.push(['trackPageView']);
